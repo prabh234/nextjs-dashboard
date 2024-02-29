a4cd013c636e0c2969428d6ca2407a7293e814a2
prabhjot singh <48572962+prabh234@users.noreply.github.com>
2024-02-29 14:18:58 +0530

    added suspence and sceletons for optimised loading

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,14 +1,30 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathName = usePathname();
+  const {replace} = useRouter();
+  function handleSearch(term:string) {
+    const params = new URLSearchParams(searchParams);
+    (term)?params.set('query',term):params.delete('query');
+    
+    replace(`${pathName}?${params.toString()}`)
+    console.log(term);
+
+    
+    
+  }
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
         Search
       </label>
       <input
+        onChange={e=>handleSearch(e.target.value)}
+        defaultValue={searchParams.get('query')?.toString()}
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
       />
